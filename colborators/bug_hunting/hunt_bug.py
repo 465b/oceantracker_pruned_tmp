dd2200f1774f72f16530eb649fab095589cf9d30
Ross Vennell <ross.vennell@cawthron.org.nz>
2023-04-13 08:08:06 +1200

    tweaks to fix multi proces issue
    Former-commit-id: 9094ba259e5b42bb157828c087bef503a0e653d6

--- colborators/bug_hunting/hunt_bug.py
+++ colborators/bug_hunting/hunt_bug.py
@@ -4,9 +4,9 @@ ncase=0
 
 if ncase ==0:
     fn = r'E:\OneDrive - Cawthron\H_Local_drive\ParticleTracking\bug_hunting\paramsBWDFY20120.json'
-    params = json_util.read_JSON(fn)
-    params['reader']['input_dir'] = r'G:\Hindcasts_large\OceanNumNZ-2022-06-20\final_version\2010'
+    params: object = json_util.read_JSON(fn)
+    params['reader']['input_dir'] = r'G:\Hindcasts_large\OceanNumNZ-2022-06-20\final_version\2012'
     params['shared_params']['root_output_dir'] = r'F:\OceanTrackerOuput\bug_hunting'
-    params['case_list'] =  params['case_list'][23:25]
-
+    params['case_list'] =  params['case_list'][23:26]
+    params['shared_params']['processors'] = 5
 main.run(params)
