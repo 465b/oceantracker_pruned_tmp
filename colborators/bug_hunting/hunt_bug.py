5bfa21bade21e21ab2f47635dd2e88b62ed689d3
Ross Vennell <ross.vennell@cawthron.org.nz>
2023-04-13 09:31:06 +1200

    got bug hunting working in parrallel
    Former-commit-id: 076f34c244fafdc2cd6bd9b7fe6e2a25a080e26e

--- colborators/bug_hunting/hunt_bug.py
+++ colborators/bug_hunting/hunt_bug.py
@@ -1,12 +1,15 @@
 from oceantracker import main
 from oceantracker.util import json_util
-ncase=0
 
-if ncase ==0:
-    fn = r'E:\OneDrive - Cawthron\H_Local_drive\ParticleTracking\bug_hunting\paramsBWDFY20120.json'
-    params: object = json_util.read_JSON(fn)
-    params['reader']['input_dir'] = r'G:\Hindcasts_large\OceanNumNZ-2022-06-20\final_version\2012'
-    params['shared_params']['root_output_dir'] = r'F:\OceanTrackerOuput\bug_hunting'
-    params['case_list'] =  params['case_list'][23:26]
-    params['shared_params']['processors'] = 5
-main.run(params)
+if __name__ == "__main__":
+    ncase=0
+
+    if ncase ==0:
+        fn = r'E:\OneDrive - Cawthron\H_Local_drive\ParticleTracking\bug_hunting\paramsBWDFY20120.json'
+        params: object = json_util.read_JSON(fn)
+        params['reader']['input_dir'] = r'G:\Hindcasts_large\OceanNumNZ-2022-06-20\final_version\2012'
+        params['shared_params']['root_output_dir'] = r'F:\OceanTrackerOuput\bug_hunting'
+        params['case_list'] =  params['case_list'][23:26]
+        params['shared_params']['processors'] = 5
+
+        main.run(params)
