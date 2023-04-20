1d32c36abfff2a19ae7de26fa6ad4749dffff9e0
Ross Vennell <ross.vennell@cawthron.org.nz>
2023-04-20 14:46:53 +1200

    fixed excessive calls to inside polygon by statistics
    Former-commit-id: 450078be3a1b6d52cbd2dfe2d9f2fef305d82974

--- colborators/bug_hunting/hunt_bug.py
+++ colborators/bug_hunting/hunt_bug.py
@@ -9,7 +9,15 @@ if __name__ == "__main__":
         params: object = json_util.read_JSON(fn)
         params['reader']['input_dir'] = r'G:\Hindcasts_large\OceanNumNZ-2022-06-20\final_version\2012'
         params['shared_params']['root_output_dir'] = r'F:\OceanTrackerOuput\bug_hunting'
-        params['case_list'] =  params['case_list'][23:26]
-        params['shared_params']['processors'] = 5
+        #params['case_list'] =  params['case_list'][23:26]
 
-        main.run(params)
+        # short run
+        params['case_list'] = [params['case_list'][25]]
+        params['shared_params']['processors'] = 1
+        #params['base_case_params']['solver']['n_substeps'] = 10
+        #params['case_list'][0]['particle_release_groups'][0]['pulse_size'] = 100
+
+
+    params['shared_params']['max_duration'] = 5*24*3600
+
+    main.run(params)
