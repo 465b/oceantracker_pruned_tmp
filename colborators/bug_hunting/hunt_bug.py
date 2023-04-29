1c4d96d192110d80248ca149461fef8e80db57d4
Ross Vennell <ross.vennell@cawthron.org.nz>
2023-04-30 10:16:12 +1200

    added up date timers
    Former-commit-id: ac84293a2863247b062a2b08741a2e9e76714d6c

--- colborators/bug_hunting/hunt_bug.py
+++ colborators/bug_hunting/hunt_bug.py
@@ -2,7 +2,7 @@ from oceantracker import main
 from oceantracker.util import json_util
 
 if __name__ == "__main__":
-    ncase=0
+    ncase=1
 
     if ncase ==0:
         fn = r'E:\OneDrive - Cawthron\H_Local_drive\ParticleTracking\bug_hunting\paramsBWDFY20120.json'
@@ -16,8 +16,14 @@ if __name__ == "__main__":
         params['shared_params']['processors'] = 1
         #params['base_case_params']['solver']['n_substeps'] = 10
         #params['case_list'][0]['particle_release_groups'][0]['pulse_size'] = 100
+    elif ncase ==1:
+        #romain
+        fn = r'E:\OneDrive - Cawthron\H_Local_drive\ParticleTracking\bug_hunting\Nydia_HABSs_2018_06_05_C001_1_caseInfo.json'
+        params: object = json_util.read_JSON(fn)
+        params['reader']['input_dir'] = r'G:\Hindcasts_large\OceanNumNZ-2022-06-20\final_version\2012'
 
 
     params['shared_params']['max_duration'] = 5*24*3600
+    params['shared_params']['root_output_dir'] = r'F:\OceanTrackerOuput\bug_hunting'
 
     main.run(params)
